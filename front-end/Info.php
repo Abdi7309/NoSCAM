8a0df174986857c130ea69069cd188a5a60a8511
TiagoJennen <113349775+TiagoJennen@users.noreply.github.com>
2023-05-15 13:21:05 +0200

    links en info

--- front-end/Info.php
+++ front-end/Info.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="home.css">
+    <title>Info</title>
+    <link rel="stylesheet" href="info.css">
     <style>
         a{
             color: black;
@@ -13,12 +13,18 @@
     </style>
 </head>
 <body>
-    <div class="container">
-          <div class="navbar">               
-            </div>   
-            <div class="row">
-            <div class="col">
-                <h1>NoScam</h1>
-                <button onclick="window.location.href='./home.php'">Home</button>
-                <button onclick="window.location.href='./index.php'">Logout</button>
-            </div>
+
+<div class="tekst">
+<p><strong>Oplichting</strong> is een vorm van bedrog waarbij iemand misleid wordt om geld, eigendommen of persoonlijke informatie af te staan aan een oplichter. Het kan op verschillende manieren plaatsvinden, zowel online als offline. Hier zijn enkele belangrijke punten om te begrijpen over oplichting:</p>
+
+<p><strong>Soorten oplichting:</strong> Er zijn talloze oplichtingsmethoden, waaronder phishing-e-mails, telefonische oplichting, identiteitsdiefstal, valse investeringen, loterijfraude, romantische zwendel, technische ondersteuningszwendel en meer. Oplichters passen hun methoden voortdurend aan en gebruiken vaak technologie om hun slachtoffers te misleiden.</p>
+
+<p><strong>Internetoplichting:</strong> Met de opkomst van internet zijn er veel nieuwe vormen van oplichting ontstaan. Bijvoorbeeld phishing, waarbij oplichters zich voordoen als legitieme organisaties om gevoelige informatie te verkrijgen, zoals wachtwoorden, creditcardgegevens of bankinformatie. Andere vormen van internetoplichting omvatten valse webwinkels, online veilingfraude, vriendschapsverzoeken op sociale media van oplichters enzovoort.</p>
+
+<p><strong>Herken de rode vlaggen:</strong> Om oplichting te voorkomen, is het belangrijk om waakzaam te zijn en de rode vlaggen te herkennen. Wees alert op ongevraagde verzoeken om persoonlijke informatie, verdachte e-mails van onbekende afzenders, aanbiedingen die te mooi lijken om waar te zijn, druk om snel te handelen, enzovoort.</p>
+
+<p><strong>Bescherming van persoonlijke informatie:</strong> Bescherming van je persoonlijke informatie is essentieel. Geef nooit gevoelige informatie, zoals wachtwoorden, creditcardgegevens of sofinummers, aan onbekende personen of onbeveiligde websites. Controleer altijd de legitimiteit van de partij waarmee je zaken doet voordat je persoonlijke informatie verstrekt.</p>
+
+<p><strong>Melding van oplichting:</strong> Als je slachtoffer bent geworden van oplichting, is het belangrijk om dit te melden bij de juiste instanties. Doe aangifte bij de politie en meld de fraude bij je bank of creditcardmaatschappij. Daarnaast kun je oplichtingspogingen melden bij instanties zoals de Fraudehelpdesk in Nederland of de Federal Trade Commission (FTC) in de Verenigde Staten.</p>
+
+    </div>
--- front-end/Links.php
+++ front-end/Links.php
@@ -4,37 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Links</title>
     <link rel="stylesheet" href="link.css">
-    <style>
-        a{
-            color: black;
-        }
-    </style>
 </head>
 <body>
-    <div class="container">
-          <div class="navbar">               
-            </div>   
-            <div class="row">
-            <div class="col">
-                <h1>NoScam</h1>
-                <button onclick="window.location.href='./home.php'">Home</button>
-                <button onclick="window.location.href='./index.php'">Logout</button>
-            </div>
-    <div id="zin">
+    <div class="zin">
         <h2>Hier een paar linkjes die je kunnen helpen!</h2 >
     </div>  
-    <div id="link1">
+    <div class="link1">
         <a href="https://www.veiliginternetten.nl/"><h2>Klik hier om hulp te vragen</h2></a>
     </div>
-    <div id="link2">
+    <div class="link2">
         <a href="https://www.checkjelinkje.nl/"><h2>Klik hier om te controleren of de link echt is</h2></a> 
     </div>
-    <div id="link3">
+    <div class="link3">
         <a href="https://www.seniorweb.nl/"><h2>Klik hier voor tips en uitleg</h2></a>  
     </div>
-    <div id="link4">
+    <div class="link4">
         <a href="https://www.safeonweb.be/"><h2>Klik hier als je op een foute link heb gedrukt</h2>
     </div>
-    
